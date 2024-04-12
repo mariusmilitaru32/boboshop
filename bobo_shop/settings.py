fea2373697d83427f2c0915a7e6d887194d1f097
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-04-12 23:30:22 +0100

    Added search and logo on index page

--- bobo_shop/settings.py
+++ bobo_shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,11 @@ ROOT_URLCONF = 'bobo_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+
+def index(request):
+    """
+    render index page
+    """
+    return render(request, 'index.html')
+
+
