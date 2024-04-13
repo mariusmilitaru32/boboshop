f60f9e9608d35c19cd1ab2efd5edb52d1c12e782
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-04-13 16:17:46 +0100

    Updated review for product detail to be float

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Review
+from django.db.models import Avg
 
 # Create your views here.
 
@@ -16,12 +17,21 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to render product details """
-
+    """A view to render product details"""
     product = get_object_or_404(Product, pk=product_id)
+    reviews = Review.objects.filter(product=product)
+    review_count = reviews.count()
+
+    if review_count > 0:
+        average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+    else:
+        average_rating = None
 
     context = {
         'product': product,
+        'reviews': reviews,
+        'review_count': review_count,
+        'average_rating': average_rating
     }
 
     return render(request, 'products/product_detail.html', context)
