301c41db694bd977f80c41f3fd78be6e2fcc98a3
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-04-13 19:36:54 +0100

    Added back to top arrow js and css

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 from django.db.models.functions import Lower
 from .models import Product, Review, Category
-from django.db.models import Avg, Count
+from django.db.models import Avg
 
 from django.db.models import Avg
 from django.contrib import messages
