3fb7376c35e75bc6b717c78709563ad78e730f4d
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-04-15 18:02:17 +0100

    FIXED add item flavour in bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,36 +4,33 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
+
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_key, item_data in bag.items():
-        if isinstance(item_data, dict):
-            product_id = item_data.get('product_id', None)
-            if product_id:
-                product = get_object_or_404(Product, pk=product_id)
-                total += item_data['quantity'] * Decimal(product.price)
-                product_count += item_data['quantity']
-                bag_items.append({
-                    'item_key': item_key,
-                    'quantity': item_data['quantity'],
-                    'product': product,
-                    'size': item_data.get('size', None),
-                })
-            else:
-                # Handle the case where 'product_id' is not present in item_data
-                pass
-        else:
-            product = get_object_or_404(Product, pk=item_key)
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
             bag_items.append({
-                'item_key': item_key,
+                'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
             })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -41,9 +38,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
