4cf00078dd800af071675cc474f40222371f462f
mariusmilitaru32 <marius.militaru20@gmail.com>
2024-04-16 22:28:03 +0100

    Updated setting for heroku

--- bobo_shop/settings.py
+++ bobo_shop/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21fpjunh355d)%poho#b+u4i2#e4t_x*67rnm1k6xwwo(z3#(#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://boboshop-4896cb751ade.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
